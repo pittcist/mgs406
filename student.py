754a2d093ea4507bf317caa80b9454211e9904ae
Ken Wang <kwang@mac2022.local>
2023-04-11 10:19:21 -0400

    Added MySQL support

--- student.py
+++ student.py
@@ -1,13 +1,19 @@
 from flask import Flask, url_for, redirect, render_template, request
 from flask_bootstrap import Bootstrap
 
-from flaskext.mysql import MySQL
-mysql = MySQL()
-mysql.init_app(app)
+app = Flask(__name__)
+Bootstrap(app)
 
 # import sqlite3 as sql
-# app = Flask(__name__)
-# Bootstrap(app)
+from flaskext.mysql import MySQL
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'db_test'
+
+mysql = MySQL()
+mysql.init_app(app)   
 
 @app.route('/')
 def home():
@@ -21,36 +27,37 @@ def new_student():
 @app.route('/addrec',methods = ['POST', 'GET'])
 def addrec():
    if request.method == 'POST':
-      try:
-         nm = request.form['nm']
-         addr = request.form['add']
-         city = request.form['city']
-         zip = request.form['zip']
-         
-         with sql.connect("db_test.db") as con:
-            cur = con.cursor()
-            cmd = "INSERT INTO students (name,addr,city,zip) VALUES ('{0}','{1}','{2}','{3}')".format(nm,addr,city,zip)
-            cur.execute(cmd)
-            
-            con.commit()
-            msg = "Record successfully added"
-      except:
-         con.rollback()
-         msg = "error in insert operation"
-         
-      finally:
-         return render_template("output.htm",msg = msg)
-         con.close()
+      nm = request.form['nm']
+      addr = request.form['add']
+      city = request.form['city']
+      zip = request.form['zip']
+      
+      # cur = mysql.connection.cursor()
+      conn = mysql.connect()
+      cur = conn.cursor()
+      cmd = "INSERT INTO students (name,addr,city,zip) VALUES ('{0}','{1}','{2}','{3}')".format(nm,addr,city,zip)
+
+      cur.execute(cmd)
+      conn.commit()
+      conn.close()
+      msg = "Successful"
+      return render_template("output.htm",msg = msg)
+
 
 @app.route('/list')
 def list():
-   con = sql.connect("db_test.db")
-   con.row_factory = sql.Row
+   # con = mysql.connect("db_test.db")
+   # con.row_factory = sql.Row
    
-   cur = con.cursor()
+   # cur = con.cursor()
+
+   conn = mysql.connect()
+   cur = conn.cursor()
    cur.execute("select * from students")
    
    rows = cur.fetchall(); 
+   print(rows)
+   conn.close()
    return render_template("list.htm",rows = rows)
 
 if __name__ == '__main__':
